63e4005db1bf0e6ee86f456e7ad6f8b134d374b6
Kirill Mangutov <kmangutov@Kirills-MacBook-Air.local>
2024-08-15 00:39:37 -0500

    api calling example

--- src/my_pipeline.py
+++ src/my_pipeline.py
@@ -3,6 +3,10 @@ import pandas as pd
 import requests
 import os
 from pipeline_lib import step, DAG
+import requests
+import time
+from datetime import datetime
+
 
 # Define cron schedule (e.g., run every hour)
 cron_schedule = "0 * * * *"  # This will run the pipeline at the start of every hour
@@ -12,11 +16,35 @@ csv_input = 'input.csv'
 csv_output = 'output.csv'
 tmp_output = 'tmp_output.csv'
 
+def fetch_data_yahoo():
+    # Define the stock ticker and date range
+    ticker = "GOOG"
+    start_date = "2023-08-14"  # One year ago from today
+    end_date = "2024-08-14"    # Today
+    # Convert dates to UNIX timestamps
+    start_timestamp = int(time.mktime(datetime.strptime(start_date, "%Y-%m-%d").timetuple()))
+    end_timestamp = int(time.mktime(datetime.strptime(end_date, "%Y-%m-%d").timetuple()))
+
+    # Yahoo Finance URL for historical data
+    url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={start_timestamp}&period2={end_timestamp}&interval=1d&events=history&includeAdjustedClose=true"
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    }
+
+    # Send GET request to Yahoo Finance with the headers
+    response = requests.get(url, headers=headers)
+    return response.text
+
 @step(cron_schedule=cron_schedule)
-def fetch_data(source=None):
+def fetch_data(source="yahoo"):
     """Fetch data from an API or CSV file."""
     print("Fetching data...")
-    return 1
+
+    data = fetch_data_yahoo()
+    print(str(data))
+
+    return data
 
 @step()
 def transform_data(df_input):
