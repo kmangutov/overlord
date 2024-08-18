00d0a4f0ca648bca5de597086695e08b51b57d9e
Kirill Mangutov <kmangutov@Kirills-MacBook-Air.local>
2024-08-18 14:34:21 -0500

    rename to pipeline

--- src/my_pipeline.py
+++ src/my_pipeline.py
@@ -2,12 +2,23 @@ import pandas as pd
 # import duckdb
 import requests
 import os
-from pipeline_lib import step, DAG, sqlite
+from pipeline_lib import step, Pipeline, sqlite
 import requests
 import time
 from datetime import datetime
 from io import StringIO
 
+
+SCHEMA_CANDLES = {
+    "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
+    "Date": "TEXT",  # or "DATE"
+    "Open": "REAL",
+    "High": "REAL",
+    "Low": "REAL",
+    "Close": "REAL",
+    "Adj_Close": "REAL",
+    "Volume": "INTEGER",
+}
 
 # Define cron schedule (e.g., run every hour)
 CRON_HOURLY = "0 * * * *"  # This will run the pipeline at the start of every hour
@@ -16,7 +27,7 @@ CRON_HOURLY = "0 * * * *"  # This will run the pipeline at the start of every ho
 def fetch_data_yahoo():
     # Define the stock ticker and date range
     ticker = "GOOG"
-    start_date = "2024-08-10"  # One year ago from today
+    start_date = "2024-08-7"  # One year ago from today
     end_date = "2024-08-14"    # Today
     # Convert dates to UNIX timestamps
     start_timestamp = int(time.mktime(datetime.strptime(start_date, "%Y-%m-%d").timetuple()))
@@ -33,8 +44,8 @@ def fetch_data_yahoo():
     response = requests.get(url, headers=headers)
     return response.text
 
-@step(cron_schedule=CRON_HOURLY) #  This could potentially be "dag.schedule(...) instead of annotation param"
-def fetch_data(source="yahoo"):
+@step()
+def fetch_data():
     """Fetch data from an API or CSV file."""
     print("Fetching data...")
     data = fetch_data_yahoo()
@@ -43,63 +54,47 @@ def fetch_data(source="yahoo"):
     return data
 
 @step()
-def transform_data(data): # **kwargs):
+def transform_data(data):
     print(f'transform_data input: {str(data)}')
-    """Transform data using DuckDB."""
-    print('transforming...' + str(data)) # TODO fix passing output to next func input
 
     df = pd.read_csv(StringIO(data))
-
     
     # Add a column for 2-day moving average of the 'Close' prices
-    df['Close 2 Day MA'] = df['Close'].rolling(window=2).mean()
-    
+    df['Signal_MA'] = df['Close'].rolling(window=2).mean()
+    df.dropna()
     return df
 
 
-def sql_schema(cursor):
-
-    default_schema = {
-        "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
-        "Date": "TEXT",  # or "DATE"
-        "Open": "REAL",
-        "High": "REAL",
-        "Low": "REAL",
-        "Close": "REAL",
-        "Adj_Close": "REAL",
-        "Volume": "INTEGER"
-    }
-
-    columns = ", ".join([f"{col} {typ}" for col, typ in default_schema.items()])
-    create_table_sql = f"CREATE TABLE IF NOT EXISTS candles ({columns});"
-    cursor.execute(create_table_sql)
-
 def sql_insert(cursor):
-        # Insert dummy data
     dummy_data = ('2024-08-12', 165.994995, 166.699997, 163.550003, 163.949997, 163.949997, 12435000)
-    cursor.execute("INSERT INTO candles (Date, Open, High, Low, Close, Adj_Close, Volume) VALUES (?, ?, ?, ?, ?, ?, ?)", dummy_data)
+
+    columns = ", ".join([col for col in SCHEMA_CANDLES.keys() if col != "id"])
+    placeholders = ", ".join(["?"] * len(dummy_data))
+
+    cursor.execute(f"INSERT INTO candles ({columns}) VALUES ({placeholders})", dummy_data)
+
 
 def sql_count(cursor):
     cursor.execute("SELECT COUNT(*) FROM candles")
     row_count = cursor.fetchone()[0]
     return row_count
 
-@sqlite('data.db')
+
+@sqlite('data.db', table_schema=SCHEMA_CANDLES)
 @step()
-def save_data(data, **kwargs): # TODO: Handle parameter from previous func AND a @sqlite conn
+def save_data(data, **kwargs):
     print('save')
 
     conn = kwargs['conn']
     cursor = conn.cursor()
-    sql_schema(cursor)
-    sql_insert(cursor)
+    sql_insert(cursor)  # TODO: Insert parameter instead of a placeholder
     count = sql_count(cursor)
     conn.commit()
 
     return count
 
-# Create a DAG
-dag = DAG()
+# Create a Pipeline
+Pipeline = Pipeline(schedule = CRON_HOURLY)
 
 # Chain the steps with `<<`
-dag << fetch_data << transform_data << save_data
+Pipeline << fetch_data << transform_data << save_data
--- src/pipeline_lib.py
+++ src/pipeline_lib.py
@@ -11,7 +11,7 @@ import sqlite3
 # TODO:
 # passing return value to next step parameter needs to work
 # make the adding to cron and removing and pritnging existing crons work
-# the cron should prboably go into a DAG member instead of task
+# the cron should prboably go into a Pipeline member instead of task
 
 class DefaultArgs:
     def __init__(self):
@@ -31,7 +31,7 @@ def snapshot_state(filename, **kwargs) -> None:
     with open(f"snapshots/{filename}_{timestamp}.pkl", "wb") as f:
         pickle.dump(obj=kwargs, file=f)
 
-def step(cron_schedule=None):
+def step():
     """Decorator to log errors, handle exceptions, and optionally set a cron schedule."""
     def decorator(func):
         print(f"Load {func.__name__}")
@@ -39,6 +39,7 @@ def step(cron_schedule=None):
         def wrapper(*args, **kwargs):
             try:
                 print(f"{func.__name__} input: {args} {kwargs}")
+                global save_snapshots
                 if save_snapshots:
                     snapshot_state(func.__name__ + "_input", kwargs=kwargs)
                 result = func(*args, **kwargs)
@@ -54,9 +55,10 @@ def step(cron_schedule=None):
 
 
 
-class DAG:
+class Pipeline:
     """Class to represent a Directed Acyclic Graph of steps."""
-    def __init__(self):
+    def __init__(self, schedule: str = None):
+        self.schedule = schedule
         self.steps = []
 
     def __lshift__(self, func):
@@ -65,26 +67,34 @@ class DAG:
         return self
 
     def run(self):
-        """Run all steps in the DAG sequentially."""
+        """Run all steps in the Pipeline sequentially."""
         if not self.steps:
-            raise ValueError("No steps to run in the DAG.")
+            raise ValueError("No steps to run in the Pipeline.")
         
         result = None
         for step_func in self.steps:
-            print(f'DAG::run result {result}')
+            print(f'Pipeline::run result {result}')
             if result is None:
                 result = step_func()
             else:
                 result = step_func(result)
 
 
-def sqlite(file_path):
+def sql_schema(cursor, schema):
+    columns = ", ".join([f"{col} {typ}" for col, typ in schema.items()])
+    create_table_sql = f"CREATE TABLE IF NOT EXISTS candles ({columns});"
+    cursor.execute(create_table_sql)
+
+
+def sqlite(file_path, table_schema = None):
     """Decorator to inject a SQLite connection."""
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             conn = sqlite3.connect(file_path)
             try:
+                if table_schema: sql_schema(conn.cursor(), table_schema)
+
                 # Inject the connection into the kwargs
                 kwargs['conn'] = conn
                 result = func(*args, **kwargs)
@@ -95,20 +105,6 @@ def sqlite(file_path):
     return decorator
 
 
-def check_errors(logfile):
-    """Check the log file for any errors."""
-    if os.path.exists(logfile):
-        with open(logfile, 'r') as f:
-            errors = f.readlines()
-        if errors:
-            print("Errors found in the log:")
-            for error in errors:
-                print(error.strip())
-        else:
-            print("No errors found in the log.")
-    else:
-        print("Log file not found.")
-
 def setup_cronjob(cron_schedule, pipeline_file) -> None:
     """Set up the cron job for the pipeline."""
     cron = CronTab(user=True)
@@ -118,12 +114,6 @@ def setup_cronjob(cron_schedule, pipeline_file) -> None:
     cron.write()
     print(f"Cron job set up: {command} at schedule '{cron_schedule}'")
 
-def list_cronjobs():
-    """List all cron jobs for the current user."""
-    cron = CronTab(user=True)
-    print("Current cron jobs:")
-    for job in cron:
-        print(f"{job.scheduled_time()} - {job.command}")
 
 def load_pipeline(pipeline_file):
     # Dynamically import the domain-specific pipeline file
@@ -146,37 +136,42 @@ def run_cli():
     parser.add_argument('--debug', action='store_true', help="Rerun last failing snapshot")
 
     # Scheduling
-    parser.add_argument('--setup-cron', action='store_true', help="Set up the cron job for this pipeline")
-    parser.add_argument('--list-cron', action='store_true', help="List all cron jobs")
+    parser.add_argument('--enable', action='store_true', help="Set up the cron job for this pipeline")
+    parser.add_argument('--disable', action='store_true', help="Remove cron job for this pipeline")
 
     # Debugging
-    parser.add_argument('--check-errors', action='store_true', help="Check for errors in the log")
-    parser.add_argument('--snapshot', action='store_true', help="Save inputs and outputs")
+    parser.add_argument('--snapshot', action='store_true', help="Run pipeline and save inputs and outputs")
 
 
     # Override default args
     args = parser.parse_args()
+
+    global save_snapshots
+    save_snapshots = args.snapshot
+
     pipeline_module =   load_pipeline(args.file)
 
     # pipeline_module = load_pipeline("./src/my_pipeline.py")
    
 
     if args.run:
-        global save_snapshots
-        save_snapshots = args.snapshot
-        pipeline_module.dag.run()
-    if args.check_errors:
-        check_errors('test.txt')
-    if args.setup_cron:
+        pipeline_module.Pipeline.run()
+    if args.debug:
+        # TODO
+        pass
+    if args.enable:
         # If a cron_schedule is provided in the pipeline file, set up the cron job
-        # TODO: cron_schedule should come as a member of DAG
-        cron_schedule = getattr(pipeline_module, 'cron_schedule', None)
+        # TODO: cron_schedule should come as a member of Pipeline
+        #cron_schedule = getattr(pipeline_module, 'cron_schedule', None)
+        cron_schedule = pipeline_module.Pipeline.schedule
         if cron_schedule:
             setup_cronjob(cron_schedule, args.file)
         else:
             print("No cron schedule found in the pipeline file.")
-    if args.list_cron:
-        list_cronjobs()
+    if args.disable:
+        # TODO
+        pass
+
 
 if __name__ == "__main__":
     run_cli()
